2cc06e7fba965fcd71a5704fd9baff3f24208966
Pratik-dev <pratikranpariya007@gmail.com>
2025-01-28 11:14:10 +0530

    Change back

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,19 +2,9 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: [
-        "*",
-        "http://localhost:3000",
-        "https://aggksok0ogk0k04gck08wkgs.cool.dynamicbusiness.com/",
-      ],
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
-    },
-  },
+
+  "strapi::cors",
+
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",

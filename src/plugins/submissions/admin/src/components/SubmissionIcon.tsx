62fc69d2367b86aea02f2f497cd511684b2d34e0
Pratik-dev <pratikranpariya007@gmail.com>
2025-01-29 09:42:31 +0530

    Changes

---
+++ src/plugins/submissions/admin/src/components/SubmissionIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SubmissionsIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24">
+    <path
+      d="M12 2L1 21h22L12 2zm0 3.84L19.11 19H4.89L12 5.84z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default SubmissionsIcon;
---
+++ src/plugins/submissions/server/routes/index.ts
@@ -0,0 +1,10 @@
+import App from "../index";
+
+const routes = [
+  {
+    path: "/plugins/submissions",
+    component: App,
+  },
+];
+
+export default routes;

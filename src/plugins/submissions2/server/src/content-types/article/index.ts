c45af622691619743a3e2be9db4608d6145d3ecc
Pratik-dev <pratikranpariya007@gmail.com>
2025-02-16 05:22:42 +0530

    Integrated the content type to Submission Plugin

--- src/plugins/submissions2/server/src/content-types/article/index.ts
+++ src/plugins/submissions2/server/src/content-types/article/index.ts
@@ -1,6 +1,12 @@
-import article from './schema.json'; // Ensure this path is correct
+import schema from './schema.json'; // Ensure this path is correct
 
 // Temporarily simplified to check if the server starts
 export default {
-  article,
+  schema,
+  kind: 'collectionType',
+  __schema__: schema, // This is important for Strapi's internal use
+  uid: 'plugin::submissions2.article', // Ensure this is correct
+  modelType: 'contentType',
+  modelName: 'article', // Change this to match singularName
+  connection: 'default',
 };

db95696913a14345ad24d55da2ad450981f77859
Pratik-dev <pratikranpariya007@gmail.com>
2025-02-22 09:36:09 +0530

    Changes

--- config/server.ts
+++ config/server.ts
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env(
-    "PUBLIC_URL",
-    "https://ig8cksgkgwkkkoos4g84w88k.cool.dynamicbusiness.com"
-  ),
-  // url: env('PUBLIC_URL', 'https://localhost:1337'),
+  // url: env(
+  //   "PUBLIC_URL",
+  //   "https://ig8cksgkgwkkkoos4g84w88k.cool.dynamicbusiness.com"
+  // ),
+  url: env("PUBLIC_URL", "https://localhost:1337"),
 });
